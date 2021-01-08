dbe1de39a80b92669fe543594868af56a897fcae
René Pascal Maseli <r.maseli@tu-braunschweig.de>
2021-01-08 14:14:33 +0100

    refactored BasicRegRelation

--- dartagnan/src/main/java/com/dat3m/dartagnan/wmm/relation/base/local/RelIdd.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/wmm/relation/base/local/RelIdd.java
@@ -4,34 +4,25 @@ import com.dat3m.dartagnan.program.Register;
 import com.dat3m.dartagnan.program.event.Event;
 import com.dat3m.dartagnan.program.event.utils.RegReaderData;
 import com.dat3m.dartagnan.program.utils.EType;
+import com.dat3m.dartagnan.wmm.filter.FilterAbstract;
 import com.dat3m.dartagnan.wmm.filter.FilterBasic;
-import com.dat3m.dartagnan.wmm.utils.TupleSet;
-import com.microsoft.z3.BoolExpr;
 
 import java.util.*;
 
 public class RelIdd extends BasicRegRelation {
 
-    public RelIdd(){
-        term = "idd";
-        forceDoEncode = true;
-    }
+	public RelIdd() {
+		term = "idd";
+		forceDoEncode = true;
+	}
 
-    @Override
-    public TupleSet getMaxTupleSet(){
-        if(maxTupleSet == null){
-            mkMaxTupleSet(program.getCache().getEvents(FilterBasic.get(EType.REG_READER)));
-        }
-        return maxTupleSet;
-    }
+	@Override
+	protected FilterAbstract filter() {
+		return FilterBasic.get(EType.REG_READER);
+	}
 
-    @Override
-    protected BoolExpr encodeApprox() {
-        return doEncodeApprox(program.getCache().getEvents(FilterBasic.get(EType.REG_READER)));
-    }
-
-    @Override
-    Collection<Register> getRegisters(Event regReader){
-        return ((RegReaderData) regReader).getDataRegs();
-    }
+	@Override
+	protected Collection<Register> getRegisters(Event regReader) {
+		return ((RegReaderData) regReader).getDataRegs();
+	}
 }
