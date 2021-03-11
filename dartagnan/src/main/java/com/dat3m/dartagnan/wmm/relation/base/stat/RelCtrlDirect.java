c109f4e0ad88b4f3c0fe86cc69817ef8118b07a2
René Pascal Maseli <r.maseli@tu-braunschweig.de>
2021-03-11 11:52:39 +0100

    add CondJump.End

--- dartagnan/src/main/java/com/dat3m/dartagnan/wmm/relation/base/stat/RelCtrlDirect.java
+++ dartagnan/src/main/java/com/dat3m/dartagnan/wmm/relation/base/stat/RelCtrlDirect.java
@@ -1,6 +1,6 @@
 package com.dat3m.dartagnan.wmm.relation.base.stat;
 
-import com.dat3m.dartagnan.program.Thread;
+import com.dat3m.dartagnan.program.event.CondJump;
 import com.dat3m.dartagnan.program.event.Event;
 import com.dat3m.dartagnan.program.event.If;
 import com.dat3m.dartagnan.program.utils.EType;
@@ -15,26 +15,32 @@ public class RelCtrlDirect extends StaticRelation {
 
 	@Override
 	protected void mkMaxTupleSet(){
-            for(Thread thread : program.getThreads()){
-                for(Event e1 : thread.getCache().getEvents(FilterBasic.get(EType.CMP))){
+		for(Event e1 : program.getCache().getEvents(FilterBasic.get(EType.CMP))){
                     for(Event e2 : ((If) e1).getMainBranchEvents()){
-					addMaxTuple(e1,e2,true);
+				addMaxTuple(e1,e2,true);
                     }
                     for(Event e2 : ((If) e1).getElseBranchEvents()){
-					addMaxTuple(e1,e2,true);
+				addMaxTuple(e1,e2,true);
                     }
-                }
+		}
 
                 List<Event> condJumps = thread.getCache().getEvents(FilterBasic.get(EType.JUMP));
                 if(!condJumps.isEmpty()){
                     for(Event e2 : thread.getCache().getEvents(FilterBasic.get(EType.ANY))){
                         for(Event e1 : condJumps){
                             if(e1.getCId() < e2.getCId()){
-							addMaxTuple(e1,e2,true);
+						addMaxTuple(e1,e2,true);
                             }
                         }
                     }
                 }
-            }
-	}
+		for(Event jump : program.getCache().getEvents(FilterBasic.get(EType.JUMP))){
+			Event label = ((CondJump)jump).getLabel();
+			assert null!=label && jump.getCId() < label.getCId();
+			for(Event e = jump.getSuccessor(); label != e; e = e.getSuccessor())
+				addMaxTuple(jump,e);
+			for(Event e = label; null != e && !(e instanceof CondJump.End); e = e.getSuccessor())
+				addMaxTuple(jump,e);
+		}
+    }
 }
